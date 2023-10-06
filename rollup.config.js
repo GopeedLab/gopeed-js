a2fbadd70643a3e48e91cdd6392537cd89087267
liwei <liwei-8466@qq.com>
2023-10-06 18:34:25 +0800

    fix: rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -5,28 +5,21 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import fs from 'node:fs';
 
-// 遍历packages目录下的所有包
+// foreach packages
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const packagesDir = path.resolve(__dirname, 'packages');
 const packages = fs.readdirSync(packagesDir).filter((f) => {
-  // 过滤掉非文件夹
   if (!fs.statSync(path.join(packagesDir, f)).isDirectory()) {
     return false;
   }
-  // 过滤掉没有package.json的文件夹
   if (!fs.existsSync(path.join(packagesDir, f, 'package.json'))) {
     return false;
   }
-  // 过滤掉没有index.ts的文件夹
   if (!fs.existsSync(path.join(packagesDir, f, 'src/index.ts'))) {
     return false;
   }
   return true;
 });
-
-// 把gopeed-model包排在第一位，因为其它包依赖了model包
-const modelIndex = packages.findIndex((pkgName) => pkgName === 'gopeed-model');
-packages.unshift(packages.splice(modelIndex, 1)[0]);
 
 export default packages.map((pkgName) => {
   const pkg = JSON.parse(fs.readFileSync(path.join(packagesDir, pkgName, 'package.json'), 'utf-8'));
